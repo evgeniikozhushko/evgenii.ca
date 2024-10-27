e4b9088eeb8373a5b461bedb7fda0dc716720a61
evgeniikozhushko <climbersrock.eugene@gmail.com>
2024-10-26 20:27:31 -0600

    Wrapped <NextUIProvider /> in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, useNavigate, useHref} from 'react-router-dom';
+import {NextUIProvider} from '@nextui-org/react';
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
@@ -12,9 +13,11 @@ document.documentElement.classList.add(storedTheme); // Apply the theme before R
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <NextUIProvider navigate={useNavigate} useHref={useHref}>
       <Provider>
         <App />
       </Provider>
+      </NextUIProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
