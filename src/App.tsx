8178d5508cf7ca167fa27c8c69a0162f5081941c
evgeniikozhushko <climbersrock.eugene@gmail.com>
2024-10-26 21:04:12 -0600

    Working Intro Page revert to erlier version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import {useNavigate} from 'react-router-dom';
+import {BrowserRouter, useNavigate, useHref} from 'react-router-dom';
 import {NextUIProvider} from '@nextui-org/react';
 
-// Lazy load components
-const IndexPage = React.lazy(() => import("@/pages/index"));
-const DocsPage = React.lazy(() => import("@/pages/docs"));
-const Intro = React.lazy(() => import("@/pages/intro"));
-// import IndexPage from "@/pages/index";
-// import DocsPage from "@/pages/docs";
-// import Intro from "@/pages/intro";
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import Intro from "@/pages/intro";
 
 function App() {
+  const navigate = useNavigate();
 
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={navigate} useHref={useHref}>
       <Routes>
         <Route path="/" element={<IndexPage />} />
+        <Route path="/docs" element={<DocsPage />}  />
         <Route path="/intro" element={<Intro />} />
-        {/* <Route path="/docs" element={<DocsPage />}  /> */}
       </Routes>
     </NextUIProvider>
   );
